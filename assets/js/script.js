6d5a5c1a68f26683dfc8cab5ef9bedc1302167c8
Dias Utsman <utsmand91@gmail.com>
2022-10-17 08:58:40 +0700

    Add slider

---
+++ assets/js/script.js
@@ -0,0 +1,17 @@
+const menuBurger = document.querySelector('.menu-toggle > *')
+const nav = document.querySelector('nav ul')
+const cover = document.querySelector('.cover')
+
+menuBurger.addEventListener('click', () => {
+    nav.classList.toggle('slide')
+    document.body.classList.toggle('stay')
+    cover.classList.toggle('show')
+});
+
+cover.addEventListener('click', () => menuBurger.click());
+
+[...nav.children].forEach(e => {
+    e.addEventListener('click', () => {
+        menuBurger.click()
+    })
+})
